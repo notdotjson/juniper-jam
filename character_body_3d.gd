a250b504068c510372693d41407d76cfba3a74df
notdotjson <dotjson413@proton.me>
2026-06-23 01:36:27 -0500

    added new cubetest map, modified rotation behavior

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -24,11 +24,11 @@ func _physics_process(delta):
 
 	if _lockout:
 		if basis != _target:
-			#basis = lerp(basis, _target, "0.05")
+			basis = lerp(basis, _target, "0.05")
 			_rotation_timer += delta
 			print("rotating!! t: ", _rotation_timer)
 		if _rotation_timer >= 1.8:
-			#basis = _target
+			basis = _target
 			_rotation_timer = 0.0
 			_lockout = false
 			print("rotation finished, lockout cleared!")
@@ -36,8 +36,8 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed(&"quit"):
 		get_tree().quit()
-	if position.y < -50:
-		position = Vector3(0, 1, 0)
+	#if position.y < -50:
+		#position = Vector3(0, 1, 0)
 	
 	var move_dir = Vector3()
 	move_dir.x = Input.get_axis(&"move_left", &"move_right")
@@ -94,8 +94,10 @@ func _input(event: InputEvent) -> void:
 		camera.rotation.x = clampf(camera.rotation.x, deg_to_rad(-70), deg_to_rad(70))
 	if !_lockout:
 		if event.is_action_pressed("rotate_map_forward"):
-			#_target = _target.rotated(Vector3.FORWARD, deg_to_rad(90))
+			_target = _target.rotated(Vector3.FORWARD, deg_to_rad(-90))
 			_lockout = true
 			print("action detected, locked out of rotation!")
+	if event.is_action_pressed("debug_respawn"):
+			global_position = Vector3(0, 0, 0)
 		
 			
